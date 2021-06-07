2536e7d5ad1b807a23ee057689aeb61aaf97eda4
bokon <bokon@HQ-TEN-ICT-31.mshome.net>
2021-06-07 19:02:03 +0100

    Initial Commit as at 2021-06-07

---
+++ src/main/java/com/accionmfb/omnix/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.accionmfb.omnix.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accionmfb/omnix/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accionmfb.omnix.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
