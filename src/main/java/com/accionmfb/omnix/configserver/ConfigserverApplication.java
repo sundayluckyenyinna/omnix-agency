fdff6c03297dcd667cd207fb93933a20c71999bd
bokon <bokon@HQ-TEN-ICT-31.mshome.net>
2021-12-17 08:33:15 +0100

    Commit as at 2021-12-17 8:22am

--- src/main/java/com/accionmfb/omnix/configserver/ConfigserverApplication.java
+++ src/main/java/com/accionmfb/omnix/configserver/ConfigserverApplication.java
@@ -1,13 +1,41 @@
 package com.accionmfb.omnix.configserver;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+@EnableEncryptableProperties
+@EnableFeignClients
 public class ConfigserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigserverApplication.class, args);
+    }
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor getPasswordEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("H*-lLo5,e:2.VN"); // encryptor's private key
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+
+        encryptor.setConfig(config);
+        return encryptor;
     }
 
 }
